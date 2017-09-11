a583f1a7ac474ee809163da2481a9f9d84e8a79f
robi-y <robi@post.jce.ac.il>
2017-09-11 12:07:22 +0300

    added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { MoviesService } from './services/movies.service';
 import { HttpModule } from '@angular/http';
 import { MoviesListComponent } from './movies-list/app.movies-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieEditorComponent } from './movie-editor/movie-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesListComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    MovieEditorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [MoviesService],
---
+++ src/app/movie-editor/movie-editor.component.ts
@@ -0,0 +1,25 @@
+import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-editor',
+  templateUrl: './movie-editor.component.html',
+  styleUrls: ['./movie-editor.component.css']
+})
+export class MovieEditorComponent implements OnInit {
+
+  movieForm = new FormGroup({
+    'name': new FormControl(null),
+    'imageUrl': new FormControl(null)
+  });
+
+  constructor() { }
+
+  onSubmit(): void {
+    console.log(this.movieForm.value);
+
+  }
+  ngOnInit() {
+  }
+
+}
